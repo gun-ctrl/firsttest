129fc39e6a880bf644ffd25b72c8ed644e1f6c76
第一次上传测试 <1578684787@qq.com>
2020-07-12 18:12:12 +0800

    Initial commit

---
+++ src/main/kotlin/MyKotlin.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var a = 10
+    for (i in 1..10){
+       // println("$i")
+    }
+    for(i in 10 downTo 1 step 2){
+        print("$i ")
+    }
+    for (i in 1 until 10){
+        print(" $i")
+    }
+}
